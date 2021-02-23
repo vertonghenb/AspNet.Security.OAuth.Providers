4f6daf97f14d2281e67255049c43a6ac5b8ff9c6
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-02-24 00:08:33 +0100

    I Hate Stylecop...

---
+++ samples/Mvc.Client/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "lalala")]
